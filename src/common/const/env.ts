e0a9b86b466b202809e20aaf1fbdbbc285caa6f3
Alex <sashaoliyar7@gmail.com>
2024-03-31 16:32:39 +0300

    setup stripe

--- src/common/const/env.ts
+++ src/common/const/env.ts
@@ -7,6 +7,17 @@ export const env = createEnv({
     GITHUB_CLIENT_ID: z.string(),
     GITHUB_CLIENT_SECRET: z.string(),
     REPLICATE_API_TOKEN: z.string(),
+    STRIPE_SECRET_KEY: z.string(),
+    STRIPE_WEBHOOK_SECRET: z.string(),
+    "50_CREDITS_PRODUCT_ID": z.string(),
+    "100_CREDITS_PRODUCT_ID": z.string(),
+    "250_CREDITS_PRODUCT_ID": z.string(),
   },
-  experimental__runtimeEnv: {},
+  client: {
+    NEXT_PUBLIC_STRIPE_PUBLISHABLE_KEY: z.string(),
+  },
+  experimental__runtimeEnv: {
+    NEXT_PUBLIC_STRIPE_PUBLISHABLE_KEY:
+      process.env.NEXT_PUBLIC_STRIPE_PUBLISHABLE_KEY,
+  },
 });
